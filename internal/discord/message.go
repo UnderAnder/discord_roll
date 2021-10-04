69291e21baf89c509868906cee5bfd936eaa9133
Under Ander <andryushin.alexander@gmail.com>
2021-10-04 21:31:05 +0700

    if to switch

--- internal/discord/message.go
+++ internal/discord/message.go
@@ -1,9 +1,7 @@
 package discord
 
 import (
-	"math/rand"
 	"strings"
-	"time"
 
 	"github.com/UnderAnder/discord_roll/internal/game"
 	"github.com/bwmarrin/discordgo"
@@ -13,38 +11,35 @@ var City string
 
 func (b *Bot) messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	nick := getMessageAuthorNick(m)
-	rand.Seed(time.Now().Unix())
 
 	// Ignore all messages created by the bot itself
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	if strings.HasPrefix(m.Content, "!roll") || strings.HasPrefix(m.Content, "!ролл") {
+
+	switch {
+	case strings.HasPrefix(m.Content, "!roll"), strings.HasPrefix(m.Content, "!ролл"):
 		roll := game.Roll(m.Content, nick)
-
 		s.ChannelMessageSend(m.ChannelID, roll)
-	}
 
-	if m.Content == "!bottle" || m.Content == "!бутылочка" {
+	case m.Content == "!bottle", m.Content == "!бутылочка":
 		randMember, err := game.Bottle(s, m)
 		if err == nil {
-			s.ChannelMessageSend(m.ChannelID, getMessageAuthorNick(m)+" :kiss: "+getMemberNick(randMember))
+			s.ChannelMessageSend(m.ChannelID, nick+" :kiss: "+getMemberNick(randMember))
 		}
-	}
 
-	if m.Content == "!score" || m.Content == "!очки" {
+	case m.Content == "!score", m.Content == "!очки":
 		score, err := b.repository.GetScore(m.Author.ID)
 		if err == nil {
-			s.ChannelMessageSend(m.ChannelID, getMessageAuthorNick(m)+" your score is "+score)
+			s.ChannelMessageSend(m.ChannelID, nick+" your score is "+score)
 		}
-	}
 
-	if strings.HasPrefix(m.Content, "!city ") || strings.HasPrefix(m.Content, "!город ") || strings.HasPrefix(m.Content, "г ") {
+	case strings.HasPrefix(m.Content, "!city "), strings.HasPrefix(m.Content, "!город "), strings.HasPrefix(m.Content, "г "):
 		str := strings.SplitN(m.Content, " ", 2)
 		city := str[1]
 		exist, _ := b.repository.CityExist(city)
 		if exist {
-			cityGame, _ := game.City(getMessageAuthorNick(m), city)
+			cityGame, _ := game.City(nick, city)
 			s.ChannelMessageSend(m.ChannelID, cityGame)
 		} else {
 			s.ChannelMessageSend(m.ChannelID, city+" такой город не существует")
--- internal/game/roll.go
+++ internal/game/roll.go
@@ -4,25 +4,32 @@ import (
 	"math/rand"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func Roll(message, nick string) string {
+	rand.Seed(time.Now().Unix())
+
 	str := strings.Split(message, " ")
 	maxScore := 100
 	quantity := 1
 	var sb strings.Builder
 
-	if len(str) > 1 {
+	switch len(str) {
+	case 1:
+		break
+	case 3:
+		var err error
+		quantity, err = strconv.Atoi(str[2])
+		if err != nil {
+			quantity = 1
+		}
+		fallthrough
+	case 2:
 		var err error
 		maxScore, err = strconv.Atoi(str[1])
 		if err != nil {
 			maxScore = 100
-		}
-		if len(str) == 3 {
-			quantity, err = strconv.Atoi(str[2])
-			if err != nil {
-				quantity = 1
-			}
 		}
 	}
 
